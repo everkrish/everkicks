703dbe602eca8b5a5cb7ff427acf47843d507490
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-09 14:52:16 -0700

    Improve markup and add nav

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,24 @@ import Home from './Home';
 function App() {
     // The state is watched by React so that it knows when to re-render the screen
     const [shoes, setShoes] = useState(["Nike", "Reebok", "Adidas"]);
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home shoes={shoes}/>} />
-                <Route path="/admin/shoes" element={<ManageShoes shoes={shoes} setShoes={setShoes}/>} />
-            </Routes>
-        </BrowserRouter>
+    return (<>
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/admin/shoes">Admin home</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home shoes={shoes}/>} />
+                        <Route path="/admin/shoes" element={<ManageShoes shoes={shoes} setShoes={setShoes}/>} />
+                    </Routes>
+                </BrowserRouter>
+            </main>
+        </>
     );
 }
 
