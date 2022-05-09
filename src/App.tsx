5ef51cb78a7b76165966c340225e98ebe20b0762
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-09 10:59:04 -0700

    Base code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 
 function App() {
-  return <h1>Everkicks</h1>;
+    const [shoes, setShoes] = useState(["Nike", "Reebok", "Adidas"]);
+    return (
+        <>
+            <h1>Everkicks</h1>
+            <ul>
+                { shoes.map((shoe) => <li>{shoe}</li>) }
+            </ul>
+        </>
+    );
 }
 
 export default App;
