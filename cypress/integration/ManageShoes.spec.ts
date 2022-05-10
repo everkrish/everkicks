17467610a2ebc71e6954ac3c97a0bb0cbcd969e8
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-10 09:12:08 -0700

    Tested navigation

--- cypress/integration/ManageShoes.spec.ts
+++ cypress/integration/ManageShoes.spec.ts
@@ -1,13 +1,17 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import React from "react";
-import App from "src/App";
 
 describe("ManageShoes", () => {
     it('should visit the manage shoes page', () => {
+        // Check input works on admin page
         cy.visit("http://localhost:3000/admin/shoes");
         cy.findByLabelText("Shoe name").type("Steve Madden");
         cy.findByRole("button", { name: "Add shoe" }).click();
         cy.findByText("Steve Madden");
         cy.findByLabelText("Shoe name").should('be.empty');
+
+        // Go to home page and see that it's been updated
+        cy.findByRole("navigation").findByRole("link", { name: "Home" }).click();
+        cy.url().should("eq", "http://localhost:3000/");
+        cy.findByLabelText("Shoe name").should('not.exist');
+        cy.findByText("Steve Madden");
     });
 })
