ceb0ba69193b071e8682363b8dcb4bcf554964f7
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-11 09:42:12 -0700

    Added touched and status

--- src/ManageShoes.tsx
+++ src/ManageShoes.tsx
@@ -2,19 +2,28 @@ import SelectInput from "src/reusable/SelectInput";
 import TextInput from "src/reusable/TextInput";
 import { ShoeList } from "src/ShoeList";
 import { Shoe } from "./types/types";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FocusEvent, useState } from "react";
 
 interface ManageShoesProps {
     shoes: Shoe[],
     setShoes: React.Dispatch<React.SetStateAction<Shoe[]>>
 }
 
-interface Errors {
-    name: string;
-    brand: string;
-    price: string;
-    size: string;
-    date: string;
+type TypeReplace<T, V> = { [K in keyof T]: T[K] extends Array<infer U> ? TypeReplace<U, V>[] : V };
+type Errors = TypeReplace<Shoe, string>;
+
+enum Status {
+    IDLE,
+    SUBMITTING,
+    SUBMITTED
+}
+
+const untouchedForm = {
+    name: false,
+    brand: false,
+    price: false,
+    size: false,
+    date: false
 }
 
 const defaultShoe: Shoe = { name: "", brand: "", size: 0, price: 0, date: "" }
@@ -22,14 +31,19 @@ const defaultShoe: Shoe = { name: "", brand: "", size: 0, price: 0, date: "" }
 function ManageShoes({ shoes, setShoes }: ManageShoesProps) {
     // The state is watched by React so that it knows when to re-render the screen
     const [newShoe, setNewShoe] = useState<Shoe>(defaultShoe);
-    const [errors, setErrors] = useState<Partial<Errors>>({});
+    const [touched, setTouched] = useState(untouchedForm);
+    const [status, setStatus] = useState<Status>(Status.IDLE);
     const { name, brand, size, price, date } = newShoe;
 
     function onChange(event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) {
         setNewShoe({ ...newShoe, [event.target.id]: event.target.value });
     }
 
-    function formIsValid() {
+    function onBlur(event: FocusEvent<HTMLInputElement> | FocusEvent<HTMLSelectElement>) {
+        setTouched({ ...touched, [event.target.id]: true });
+    }
+
+    function validate() {
         const currentErrors: Partial<Errors> = {};
         if (! newShoe.brand) {
             currentErrors.brand = "Brand is required."
@@ -46,67 +60,80 @@ function ManageShoes({ shoes, setShoes }: ManageShoesProps) {
         if (! newShoe.date) {
             currentErrors.date = "Date is required."
         }
-        setErrors(currentErrors);
-        return Object.keys(currentErrors).length === 0;
+        return currentErrors;
     }
 
-    return (
+    const errors = validate();
+    const isValid = Object.keys(errors).length === 0;
+
+    function getErrorForName(name: keyof Shoe) {
+        return touched[name] || status === Status.SUBMITTED
+            ? errors[name]
+            : undefined;
+    }
+        return (
         <>
             <h1>Everkicks: Manage Shoes</h1>
             <section>
                 <h2>Add Shoe</h2>
                 <form onSubmit={(event) => {
                     event.preventDefault();
-                    if (! formIsValid()) {
+                    setStatus(Status.SUBMITTING);
+                    if (! isValid) {
+                        setStatus(Status.SUBMITTED);
                         return;
                     }
                     setShoes([...shoes, newShoe]);
                     setNewShoe(defaultShoe);
+                    setTouched(untouchedForm);
                 }}>
                     <SelectInput
-                        id="brand" value={brand} label="Brand" onChange={onChange}
+                        id="brand" value={brand} label="Brand" onChange={onChange} onBlur={onBlur}
                         options={[
                             { value: "", label: "Select brand" },
                             { value: "Nike", label: "Nike" },
                             { value: "Adidas", label: "Adidas" },
                             { value: "British Knights", label: "British Knights" }
                         ]}
-                        error={errors.brand}
+                        error={getErrorForName("brand")}
                     />
                     <TextInput
                         id="name"
                         value={name}
                         label="Shoe name"
                         onChange={onChange}
-                        onBlur={}
+                        onBlur={onBlur}
                         type="text"
-                        error={errors.name}
+                        error={getErrorForName("name")}
                     />
                     <TextInput
                         id="size"
                         value={size}
                         label="Shoe size"
                         onChange={onChange}
+                        onBlur={onBlur}
                         type="number"
                         step="0.5"
-                        error={errors.size}
+                        error={getErrorForName("size")}
                     />
                     <TextInput
                         id="price"
                         value={price}
                         label="Price"
                         onChange={onChange}
+                        onBlur={onBlur}
                         type="number"
                         step="0.01"
-                        error={errors.price}
+                        error={getErrorForName("price")}
                     />
                     <TextInput
                         id="date"
                         value={date}
                         label="Release Date"
                         onChange={onChange}
+                        onBlur={onBlur}
                         type="date"
-                        error={errors.date}
+                        error={getErrorForName("date")}
                     />
                     <button type="submit">Add shoe</button>
                 </form>
--- src/reusable/SelectInput.tsx
+++ src/reusable/SelectInput.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, FocusEventHandler } from "react";
 
 interface TextInputProps {
     /** Unique id */
@@ -13,9 +13,11 @@ interface TextInputProps {
     error?: string;
     /** On change handler */
     onChange: ChangeEventHandler<HTMLSelectElement>;
+    /** On blur handler */
+    onBlur?: FocusEventHandler<HTMLSelectElement>;
 }
 
-export default function SelectInput({ id, value, label, options, error, onChange }: TextInputProps) {
+export default function SelectInput({ id, value, label, options, error, onChange, onBlur }: TextInputProps) {
     const additionalInputProps: any = {};
     if (error) {
         additionalInputProps["aria-invalid"] = true;
@@ -30,6 +32,7 @@ export default function SelectInput({ id, value, label, options, error, onChange
                     id={id}
                     value={value}
                     onChange={onChange}
+                    onBlur={onBlur}
                     {...additionalInputProps}
                 >
                     { options.map((option) => <option value={option.value} key={option.value}>{option.label}</option>) }
