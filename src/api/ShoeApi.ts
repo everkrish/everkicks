c18a60ad44b15a156f09beed2b243ac78ab4ef15
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-11 12:02:37 -0700

    Added API calls file

---
+++ src/api/ShoeApi.ts
@@ -0,0 +1,25 @@
+import { DbShoe, Shoe } from "src/types/types";
+
+const API_URI = "http://localhost:3001";
+
+export async function getShoes(): Promise<Shoe[]> {
+    const response = await fetch(`${API_URI}/shoes`);
+    if (! response.ok) {
+        throw response;
+    }
+    return await response.json();
+}
+
+export async function addShoe(shoe: Shoe): Promise<Shoe> {
+    const response = await fetch(`${API_URI}/shoes`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(shoe)
+    });
+    if (! response.ok) {
+        throw response;
+    }
+    return await response.json();
+}
