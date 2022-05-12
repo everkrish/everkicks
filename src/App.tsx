5c5f9e1a3424156f5999bb2901ce039d0e837f14
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-12 10:49:22 -0700

    Consumed devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { getShoes } from "src/api/ShoeApi";
+import DevTools, { userType } from "src/DevTools";
 import { DbShoe } from "./types/types";
 import ManageShoes from "./ManageShoes";
 import Home from './Home';
@@ -15,6 +16,7 @@ function App() {
     // The state is watched by React so that it knows when to re-render the screen
     const [shoes, setShoes] = useState<DbShoe[]>([]);
     const [status, setStatus] = useState<Status>(Status.IDLE);
+    const [user, setUser] = useState(userType.CUSTOMER);
     useEffect(() => {
         async function getShoesWrapper() {
             setStatus(Status.LOADING);
@@ -32,17 +34,23 @@ function App() {
                 <nav>
                     <ul>
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/admin/shoes">Admin home</Link></li>
+                        { (user === userType.ADMIN) && <li><Link to="/admin/shoes">Admin home</Link></li> }
                     </ul>
                 </nav>
             </header>
             <main>
                     <Routes>
                         <Route path="/" element={<Home shoes={shoes} isLoading={status !== Status.LOADED}/>} />
-                        <Route path="/admin/shoes" element={<ManageShoes shoes={shoes} setShoes={setShoes} isLoading={status !== Status.LOADED}/>} />
+                        {
+                            (user === userType.ADMIN)
+                            && <Route path="/admin/shoes"
+                                element={<ManageShoes shoes={shoes} setShoes={setShoes}
+                                                      isLoading={status !== Status.LOADED}/>}/>
+                        }
                     </Routes>
             </main>
             </BrowserRouter>
+            <DevTools user={user} setUser={setUser}/>
         </>
     );
 }
